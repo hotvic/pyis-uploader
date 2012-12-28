5c30015743903c919fa5d11a6fba2d0363eb1262
Victor Aurélio <victor.feradecs@gmail.com>
2012-12-28 14:24:06 -0300

    updated install.sh

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ for i in $@; do
 		cat << HELP
 Usage: ./install.sh [Options]
 Options:
-  --prefix    :       Installation prefix
-  --help      :       Show this help message
+  --prefix  :       Installation prefix
+  --help    :       Show this help message
 HELP
 	fi
 done
@@ -29,6 +29,21 @@ error(){
 	exit 1
 }
 echo "Installing..."
+## Install executable
+
+if [ ! -d ${PREFIX}/bin/ ]; then
+	install -d ${PREFIX}/bin/
+fi
+echo "===> Installing ${PREFIX}/bin/pyih-uploader ..."
 install -Dm=755 pyih-uploader.py ${PREFIX}/bin/pyih-uploader || error
+## Install locale
+
+if [ ! -d ${PREFIX}/share/locale ];then
+	install -d ${PREFIX}/share/locale
+fi
+for dir in $(ls locale); do
+	echo "===> Installing ${PREFIX}/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo ..."
+	install locale/${dir}/LC_MESSAGES/pyih-uploader.mo ${PREFIX}/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo || error
+done
 echo "Installation successfully"
 
