a2b6a50deffd34c28928588b56ceacb525eea539
Victor Aurélio <victor.feradecs@gmail.com>
2013-01-25 00:19:38 -0300

    now read DESTDIR var

--- install.sh
+++ install.sh
@@ -13,78 +13,90 @@ error(){
 }
 
 install_app(){
-	if [ $UID -ne 0 ]; then
-		echo "You need root access to install this program, exiting..."
+	if [ -z $DESTDIR ]; then
+		DESTDIR=$1
+	else
+		msg2 "Using $DESTDIR as destination directory..."
+	fi
+
+	if [ -d $DESTDIR ] && [ ! -w $DESTDIR ]; then
+		echo "You no have permission to write to $DESTDIR"
 		exit 1
 	fi
 
 	msg2 "Installing..."
 	## Install executable
 
-	if [ ! -d $1/bin/ ]; then
-		install -d $1/bin/
+	if [ ! -d $DESTDIR/bin/ ]; then
+		install -d $DESTDIR/bin/
 	fi
-	if [ ! -d $1/lib/ ]; then
-		install -d $1/lib/pyih-uploader
+	if [ ! -d $DESTDIR/lib/ ]; then
+		install -d $DESTDIR/lib/pyih-uploader
 	fi
-	msg3 "Installing $1/bin/pyih-uploader ..."
-	echo "#!/bin/sh" > $1/bin/pyih-uploader
-	echo "cd $1/lib/pyih-uploader/" >> $1/bin/pyih-uploader
-	echo 'python2 pyih-uploader.py $@' >> $1/bin/pyih-uploader
-	chmod 755 $1/bin/pyih-uploader
-	msg3 "Installing $1/lib/pyih-uploader/pyih-uploader.py ..."
-	install -Dm=644 pyih-uploader.py $1/lib/pyih-uploader/pyih-uploader.py || error
-	msg3 "Installing $1/lib/pyih-uploader/utils.py ..."
-	install -Dm=644 utils.py $1/lib/pyih-uploader/utils.py || error
+	msg3 "Installing $DESTDIR/bin/pyih-uploader ..."
+	echo "#!/bin/sh" > $DESTDIR/bin/pyih-uploader
+	echo "cd $DESTDIR/lib/pyih-uploader/" >> $DESTDIR/bin/pyih-uploader
+	echo 'python2 pyih-uploader.py $@' >> $DESTDIR/bin/pyih-uploader
+	chmod 755 $DESTDIR/bin/pyih-uploader
+	msg3 "Installing $DESTDIR/lib/pyih-uploader/pyih-uploader.py ..."
+	install -Dm=644 pyih-uploader.py $DESTDIR/lib/pyih-uploader/pyih-uploader.py || error
+	msg3 "Installing $DESTDIR/lib/pyih-uploader/utils.py ..."
+	install -Dm=644 utils.py $DESTDIR/lib/pyih-uploader/utils.py || error
 
 	## Install locale
 
-	if [ ! -d $1/share/locale ];then
-		install -d $1/share/locale
+	if [ ! -d $DESTDIR/share/locale ];then
+		install -d $DESTDIR/share/locale
 	fi
 	for dir in $(ls locale); do
-		msg3 "Installing $1/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo ..."
-		if [ ! -d $1/share/locale/${dir}/LC_MESSAGES/ ]; then
-			install -d $1/share/locale/${dir}/LC_MESSAGES/
+		msg3 "Installing $DESTDIR/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo ..."
+		if [ ! -d $DESTDIR/share/locale/${dir}/LC_MESSAGES/ ]; then
+			install -d $DESTDIR/share/locale/${dir}/LC_MESSAGES/
 		fi
-		install locale/${dir}/LC_MESSAGES/pyih-uploader.mo $1/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo || error
+		install locale/${dir}/LC_MESSAGES/pyih-uploader.mo $DESTDIR/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo || error
 	done
 
 	## Install man page
-	msg3 "Installing man page to $1/share/man/man1/ ..."
-	if [ ! -d $1/share/man/man1 ]; then
-		install -d $1/share/man/man1
+	msg3 "Installing man page to $DESTDIR/share/man/man1/ ..."
+	if [ ! -d $DESTDIR/share/man/man1 ]; then
+		install -d $DESTDIR/share/man/man1
 	fi
-	install -g 0 -o 0 -m 0644 docs/pyih-uploader.1 $1/share/man/man1/ || error
-	gzip $1/share/man/man1/pyih-uploader.1 || error
+	install -m 0644 docs/pyih-uploader.1 $DESTDIR/share/man/man1/ || error
+	gzip $DESTDIR/share/man/man1/pyih-uploader.1 || error
 
 	msg2 "Installation successfully"
 }
 
 uninstall_app(){
-	if [ $UID -ne 0 ]; then
-		echo "You need root access to install this program, exiting..."
+	if [ -z $DESTDIR ]; then
+		DESTDIR=$1
+	else
+		msg2 "Using $DESTDIR as destination directory ..."
+	fi
+
+	if [ -d $DESTDIR ] && [ ! -w $DESTDIR ]; then
+		echo "You no have permission to write to $DESTDIR"
 		exit 1
 	fi
 
 	msg2 "Uninstalling..."
 	## uninstall executable
 
-	if [ -f $1/bin/pyih-uploader ]; then
-		msg3 "Uninstalling $1/bin/pyih-uploader ..."
-		rm $1/bin/pyih-uploader
+	if [ -f $DESTDIR/bin/pyih-uploader ]; then
+		msg3 "Uninstalling $DESTDIR/bin/pyih-uploader ..."
+		rm $DESTDIR/bin/pyih-uploader
 	else
-		msg3 "Warning: Unable to find $1/bin/pyih-uploader, ignoring..."
+		msg3 "Warning: Unable to find $DESTDIR/bin/pyih-uploader, ignoring..."
 	fi
 
 	## Uninstall locale
 
 	for dir in $(ls locale); do
-		msg3 "Uninstalling $1/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo ..."
-		if [ -f $1/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo ];then
-			rm $1/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo
+		msg3 "Uninstalling $DESTDIR/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo ..."
+		if [ -f $DESTDIR/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo ];then
+			rm $DESTDIR/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo
 		else
-			msg3 "Warning: Unable to find $1/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo, ignoring..."
+			msg3 "Warning: Unable to find $DESTDIR/share/locale/${dir}/LC_MESSAGES/pyih-uploader.mo, ignoring..."
 		fi
 	done
 
