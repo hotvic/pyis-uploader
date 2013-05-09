68f015f33d509db36945be8b8c7b2d5c5696b502
Victor Aurélio <victoraur.santos@gmail.com>
2013-05-08 22:28:27 -0300

    working on fixed configuration (using json)

--- config.py
+++ config.py
@@ -17,22 +17,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-class O:
+import os
+import json
+
+DEFAULTS = {
+    'IMAGESHACK_KEY'  : "47DHIJMSe847793024d16f9db3e6f7b0d31389cc",
+	'ONLY_PRINT_URL'  : False,
+	'ONLY_PRINT_THB'  : False,
+    'PRINT_FULL_IN_M' : False,
+	'SEND_CLIPBOARD'  : False,
+	'RESIZE_IMAGE'    : False,
+	'USER_USER'       : False,
+	'USER_PASSWORD'   : False,
+	'USER_COOKIE'     : False,
+    'IMAGE_TAGS'      : False,
+	'PYGTK_MODULE'    : False,
+	'VERBOSE_OUTPUT'  : False
+}
+
+class O(object):
 	def __init__(self):
-		self.opts = {
-			'IMAGESHACK_KEY'  : "47DHIJMSe847793024d16f9db3e6f7b0d31389cc",
-			'ONLY_PRINT_URL'  : False,
-			'ONLY_PRINT_THB'  : False,
-            'PRINT_FULL_IN_M' : False,
-			'SEND_CLIPBOARD'  : False,
-			'RESIZE_IMAGE'    : False,
-			'USER_USER'       : False,
-			'USER_PASSWORD'   : False,
-			'USER_COOKIE'     : False,
-            'IMAGE_TAGS'      : False,
-			'PYGTK_MODULE'    : False,
-			'VERBOSE_OUTPUT'  : False
-		}
+		self.opts = DEFAULTS
 	def getopt(self, optname):
 		return self.opts[optname]
 	def getopts(self):
@@ -41,3 +46,41 @@ class O:
 		self.opts[optname] = value
 	def save(self):
 		pass
+
+class Config(object):
+    def __init__(self):
+        """ Create a new 'Config' object """
+        self._tempdict = {}
+        self._cfile = None
+        self._opts = DEFAULTS
+
+    def _load(self):
+        """ Private function for load config file """
+        cfgdir = os.getenv('XDG_CONFIG_HOME', os.getenv('HOME') + '/.config') + '/pyis-uploader'
+        cfgfile = cfgdir + '/config.json'
+        if os.path.ifdir(cfgdir) and os.path.isfile(cfgfile):
+            self._cfile = open(cfgfile, 'rw')
+        else os.path.exists(cfgdit) and os.path.isdir(cfgdir):
+            self._cfile = open(cfgfile, 'w')
+        elif not os.path.exits(cfgdir):
+            os.mkdir(cfgdir)
+
+    def _parsejson(self):
+        """ Private function for manipulate and parse json (read-only) """
+        pass
+
+    def _writejson(se lf):
+        """ Private function for manipulate and dumps json (write-only) """
+        pass
+
+    def getopt(self, opt):
+        """ Return an option value """
+        pass
+ 
+    def setopt(self, opt, value):
+        """ Set an option value (doesn't write yet!) """
+        pass
+
+    def getopt_all(self):
+        """ Return all options and respectively values """
+        pass
