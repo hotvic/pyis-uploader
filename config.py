d95f5832f65180b5424cd46faea87d30a58d651a
Victor Aurélio <victoraur.santos@gmail.com>
2013-05-13 19:01:51 -0300

    config.py POSIX Config class done

--- config.py
+++ config.py
@@ -35,52 +35,76 @@ DEFAULTS = {
 	'VERBOSE_OUTPUT'  : False
 }
 
-class O(object):
+class FBConfig(object):
 	def __init__(self):
 		self.opts = DEFAULTS
 	def getopt(self, optname):
 		return self.opts[optname]
-	def getopts(self):
+	def getopt_all(self):
 		return self.opts
 	def setopt(self, optname, value = None):
 		self.opts[optname] = value
-	def save(self):
+	def close(self):
 		pass
 
-class Config(object):
+class PConfig(object):
     def __init__(self):
         """ Create a new 'Config' object """
         self._tempdict = {}
         self._cfile = None
         self._opts = DEFAULTS
+        self._load()
+        self._parsejson()
 
     def _load(self):
         """ Private function for load config file """
-        cfgdir = os.getenv('XDG_CONFIG_HOME', os.getenv('HOME') + '/.config') + '/pyis-uploader'
-        cfgfile = cfgdir + '/config.json'
-        if os.path.ifdir(cfgdir) and os.path.isfile(cfgfile):
-            self._cfile = open(cfgfile, 'rw')
-        else os.path.exists(cfgdit) and os.path.isdir(cfgdir):
-            self._cfile = open(cfgfile, 'w')
-        elif not os.path.exits(cfgdir):
-            os.mkdir(cfgdir)
+        self.cfgdir = os.getenv('XDG_CONFIG_HOME', os.getenv('HOME') + '/.config') + '/pyis-uploader'
+        self.cfgfile = self.cfgdir + '/config.json'
+        if os.path.isdir(self.cfgdir) and os.path.isfile(self.cfgfile):
+            with open(self.cfgfile, 'r') as f:
+                self._cfile = f.read()
+        elif not os.path.exists(self.cfgdir):
+            os.mkdir(self.cfgdir)
 
     def _parsejson(self):
         """ Private function for manipulate and parse json (read-only) """
-        pass
+        if self._cfile == None:
+            self._tempdict = self._opts
+        else:
+            self._tempdict = json.loads(self._cfile)
 
-    def _writejson(se lf):
+    def _writejson(self):
         """ Private function for manipulate and dumps json (write-only) """
-        pass
+        with open(self.cfgfile, 'w') as f:
+            dump = json.dumps(self._tempdict, sort_keys=True, indent=4, separators=(',', ': '))
+            f.write(dump)
 
     def getopt(self, opt):
         """ Return an option value """
-        pass
+        if opt in self._tempdict:
+            return self._tempdict[opt]
+        elif not opt in self._tempdict and opt in self._opts:
+            return self._opts[opt]
+        else:
+            return False
  
     def setopt(self, opt, value):
         """ Set an option value (doesn't write yet!) """
-        pass
+        if opt in self._opts:
+            self._tempdict[opt] = value
+        else:
+            return False
 
     def getopt_all(self):
         """ Return all options and respectively values """
-        pass
+        return self._tempdict
+
+    def close(self):
+        """ Write changes to configuration file """
+        self._writejson()
+
+def Config():
+    if os.name == "posix":
+        return PConfig()
+    else:
+        return FBConfig()
